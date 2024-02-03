561b8d1e2192cf9af1dc74286553e71bbb04ec03
kutaui <kutaybekleric@gmail.com>
2024-02-04 00:04:36 +0300

    feat: finish header

--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -22,36 +22,114 @@
 </script>
 
 <header>
-	<h3>Kutay</h3>
+	<h3>
+		<a href="/">
+			Kutay
+		</a>
+	</h3>
 	<nav>
 		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="about.php">About</a></li>
-			<li><a href="contact.php">Contact</a></li>
+			<li><a href="/categories">Categories</a></li>
 		</ul>
-	</nav>
-	<form
-		method="POST"
-		action="/?/theme"
-		use:enhance={async () => {
+		<form
+			method="POST"
+			action="/?/theme"
+			use:enhance={async () => {
 				$theme = nextTheme
 
 				return async ({result}) => {
 					await applyAction(result)
 				}
 			}}
-	>
-		<input name="theme" value={nextTheme} hidden />
-		<button class="button">
-			<ThemeToggleIcon />
-		</button>
-	</form>
-
+		>
+			<input name="theme" value={nextTheme} hidden />
+			<button >
+				<ThemeToggleIcon />
+			</button>
+		</form>
+	</nav>
 </header>
 
 <style>
-	.button {
-			width: 40px;
-			cursor: pointer;
-	}
+    button {
+        width: 40px;
+        cursor: pointer;
+    }
+
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem;
+        background-color: var(--background-color);
+        color: var(--text-color);
+        margin: 0 auto 3rem auto;
+    }
+
+    ul {
+        display: flex;
+        gap: 1rem;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+
+    nav {
+        display: flex;
+        align-items: center;
+        gap: 3rem;
+    }
+
+
+    a {
+        color: inherit;
+        text-decoration: none;
+        display: block;
+        position: relative;
+        padding: 0.2em 0;
+        font-size: 1.5rem;
+        font-family: 'JetBrains Mono', monospace;
+
+    }
+
+
+    li {
+        margin: 0 1.5rem;
+    }
+
+    h3 a {
+        font-size: 2.5rem;
+    }
+
+    a::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 0.13em;
+        background-color: var(--text-color);
+        opacity: 0;
+        transition: opacity 350ms, transform 350ms;
+    }
+
+    a:hover::after,
+    a:focus::after {
+        opacity: 1;
+        transform: translate3d(0, 0.2em, 0);
+    }
+
+    li a {
+        overflow: hidden;
+    }
+
+    li a::after {
+        opacity: 1;
+        transform: translate3d(-102%, 0, 0);
+    }
+
+    li a:hover::after,
+    li a:focus::after {
+        transform: translate3d(0, 0, 0);
+    }
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,6 @@
 	import '../app.css';
 	import Header from '$lib/components/Header/Header.svelte';
 	import { theme } from '$lib/stores/theme';
-	import {page} from '$app/stores'
 	import {browser} from '$app/environment'
 	import type { LayoutServerData } from '../../.svelte-kit/types/src/routes/$types';
 
