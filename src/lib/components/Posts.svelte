dd3c09712774abe1593e1b92d41800a123e541e3
kutaui <kutaybekleric@gmail.com>
2024-02-06 12:27:09 +0300

    feat: make components

---
+++ src/lib/components/Posts.svelte
@@ -0,0 +1,91 @@
+<script lang="ts">
+	import { formatDate } from '$lib/utils';
+	import Tag from '$lib/components/Tag.svelte';
+	import type { Post } from '$lib/types';
+
+	export let posts: Post[] = [];
+</script>
+
+
+<ul class="posts">
+	{#each posts as post}
+		<li class="post">
+			<a href="/{post.slug}" class="title">{post.title}</a>
+			<div class="categories">
+				<p class="date">{formatDate(post.date)}</p>
+				{#each post.categories as category}
+					<Tag link="/categories/{category}" title="#{category}" />
+				{/each}
+			</div>
+			<p class="description">{post.description}</p>
+		</li>
+	{/each}
+</ul>
+
+<style>
+    .posts {
+        list-style: none;
+        padding: 0;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .post {
+        margin-bottom: 2rem;
+
+    }
+
+    .title {
+        width: 100%;
+        position: relative;
+        padding: 0.2em 0;
+        font-size: 2.8rem;
+        font-family: 'JetBrains Mono', monospace;
+        font-weight: 600;
+        color: inherit;
+        text-decoration: none;
+    }
+
+    .categories {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-items: center;
+        gap: 0.5em;
+        margin: 0.5rem 0;
+
+    }
+
+    .category {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 9999px;
+        padding: 0.2em 0.5em;
+        color: var(--text-color-soft);
+        text-decoration: none;
+        border: 1px solid var(--text-color);
+        font-size: 1.1rem;
+        font-weight: 600;
+        font-family: 'Merriweather', serif;
+    }
+
+    .description {
+        font-size: 1.6rem;
+        color: var(--text-color-soft);
+
+        margin: 1rem 0;
+        width: 85%;
+        font-family: 'Merriweather', serif;
+    }
+
+    .date {
+        font-size: 1.2rem;
+        color: var(--text-color-soft);
+        font-weight: 600;
+    }
+
+    a:hover {
+        opacity: 0.7;
+    }
+</style>
--- src/routes/categories/[slug]/+page.svelte
+++ src/routes/categories/[slug]/+page.svelte
@@ -1,97 +1,16 @@
 <script lang="ts">
-	import { formatDate } from '$lib/utils';
-	import Tag from '$lib/components/Tag.svelte';
+	import Posts from '$lib/components/Posts.svelte';
+	import { page } from '$app/stores';
 
 	export let data;
-	console.log(data)
-</script>
 
+	$: tagName = $page.url.pathname.split('/').pop();
+
+</script>
 <svelte:head>
-	<title>Kutay's Blog</title>
+	<title>Categories | {tagName}</title>
 </svelte:head>
 
 <section>
-	<ul class="posts">
-		{#each data.posts as post}
-			<li class="post">
-				<a href="/{post.slug}" class="title">{post.title}</a>
-				<div class="categories">
-					<p class="date">{formatDate(post.date)}</p>
-					{#each post.categories as category}
-						<Tag link="/categories/{category}" title="#{category}" />
-					{/each}
-				</div>
-				<p class="description">{post.description}</p>
-			</li>
-		{/each}
-	</ul>
+	<Posts posts={data.posts}/>
 </section>
-
-<style>
-    section {
-        margin: 0 auto;
-    }
-
-    .posts {
-        list-style: none;
-        padding: 0;
-        display: flex;
-        flex-direction: column;
-    }
-
-    .post {
-        margin-bottom: 2rem;
-    }
-
-    .title {
-        width: 100%;
-        position: relative;
-        padding: 0.2em 0;
-        font-size: 2.8rem;
-        font-family: 'JetBrains Mono', monospace;
-        font-weight: 600;
-        color: inherit;
-        text-decoration: none;
-    }
-
-    .categories {
-        display: flex;
-        flex-wrap: wrap;
-        align-items: center;
-        justify-items: center;
-        gap: 0.5em;
-        margin: 0.5rem 0;
-    }
-
-    .category {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        border-radius: 9999px;
-        padding: 0.2em 0.5em;
-        color: var(--text-color-soft);
-        border: 1px solid var(--text-color);
-        font-size: 1.1rem;
-        font-weight: 600;
-        font-family: 'Merriweather', serif;
-    }
-
-    .description {
-        font-size: 1.6rem;
-        color: var(--text-color-soft);
-
-        margin: 0.5rem 0;
-        width: 85%;
-        font-family: 'Merriweather', serif;
-    }
-
-    .date {
-        font-size: 1.2rem;
-        color: var(--text-color-soft);
-        font-weight: 600;
-    }
-
-    a:hover {
-        opacity: 0.7;
-    }
-</style>
