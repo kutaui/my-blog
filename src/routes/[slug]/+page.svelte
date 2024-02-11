a5d9898e8552a29250411e0981ecaa97e6f86b8d
kutaui <kutaybekleric@gmail.com>
2024-02-11 20:29:09 +0300

    feat: change title

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -6,13 +6,13 @@
 </script>
 
 <svelte:head>
-	<title>{data.meta.title}</title>
+	<title>Kutay | {data.meta.title}</title>
 	<meta property="og:type" content="article" />
 	<meta property="og:title" content={data.meta.title} />
 </svelte:head>
 
 <article>
-		<h1>{data.meta.title}</h1>
+	<h1>{data.meta.title}</h1>
 
 	<div class="tags">
 		<p>{formatDate(data.meta.date)}</p>
@@ -27,63 +27,62 @@
 </article>
 
 <style>
-    article {
-        margin-inline: auto;
-        width: 80%;
-        margin: 0 auto;
-    }
+	article {
+		margin-inline: auto;
+		width: 80%;
+		margin: 0 auto;
+	}
 
-    h1 {
-        text-transform: capitalize;
-        width: 100%;
-        position: relative;
-        padding: 0.2em 0;
-        font-size: 3.5rem;
-        font-family: 'JetBrains Mono', monospace;
-        font-weight: 600;
-        color: inherit;
-        text-decoration: none;
-    }
+	h1 {
+		text-transform: capitalize;
+		width: 100%;
+		position: relative;
+		padding: 0.2em 0;
+		font-size: 3.5rem;
+		font-family: 'JetBrains Mono', monospace;
+		font-weight: 600;
+		color: inherit;
+		text-decoration: none;
+	}
 
-    .tags {
-        display: flex;
-        flex-wrap: wrap;
-        align-items: center;
-        justify-items: center;
-        gap: 0.5em;
-        margin: 0.5rem 0 2rem 0;
-    }
+	.tags {
+		display: flex;
+		flex-wrap: wrap;
+		align-items: center;
+		justify-items: center;
+		gap: 0.5em;
+		margin: 0.5rem 0 2rem 0;
+	}
 
-    .tags p {
-        font-size: 1.5rem;
-        color: var(--text-color-soft);
-        margin-right: 2rem;
-    }
+	.tags p {
+		font-size: 1.5rem;
+		color: var(--text-color-soft);
+		margin-right: 2rem;
+	}
 
-    .prose {
-        font-family: 'Merriweather', serif;
-        font-size: 1.2rem;
-        line-height: 2;
-        color: var(--text-color-soft);
-		    margin-bottom: 5rem;
-    }
+	.prose {
+		font-family: 'Merriweather', serif;
+		font-size: 1.2rem;
+		line-height: 2;
+		color: var(--text-color-soft);
+		margin-bottom: 5rem;
+	}
 
-    @media screen and (max-width: 768px) {
-				article {
-						width: 90%;
-				}
-
-				h1 {
-						font-size: 2.5rem;
-				}
-
-				.tags p {
-						font-size: 1.2rem;
-				}
-
-				.prose {
-						font-size: 1rem;
-				}
+	@media screen and (max-width: 768px) {
+		article {
+			width: 90%;
 		}
 
+		h1 {
+			font-size: 2.5rem;
+		}
+
+		.tags p {
+			font-size: 1.2rem;
+		}
+
+		.prose {
+			font-size: 1rem;
+		}
+	}
 </style>
--- src/routes/categories/+page.svelte
+++ src/routes/categories/+page.svelte
@@ -2,20 +2,19 @@
 	import Tag from '$lib/components/Tag.svelte';
 
 	export let data;
-
 </script>
 
 <svelte:head>
-	<title>Categories</title>
+	<title>Kutay | Categories</title>
 </svelte:head>
 
 <section>
 	<h3>Categories</h3>
 	<div>
-		{#each data.posts as post,index}
+		{#each data.posts as post, index}
 			{#each post.categories as category}
 				{#if !data.posts
-					.flatMap(p => p.categories)
+					.flatMap((p) => p.categories)
 					.slice(0, index)
 					.includes(category)}
 					<Tag link="/categories/{category}" title="#{category}" />
@@ -24,35 +23,35 @@
 		{/each}
 	</div>
 </section>
+
 <style>
-    section {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-    }
+	section {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
 
-    h3 {
-        font-family: 'JetBrains Mono', monospace;
-        font-weight: 600;
-        font-size: 3rem;
-        margin-bottom: 2rem;
-    }
+	h3 {
+		font-family: 'JetBrains Mono', monospace;
+		font-weight: 600;
+		font-size: 3rem;
+		margin-bottom: 2rem;
+	}
 
-    div {
-        display: flex;
-        gap: 1rem;
-        flex-wrap: wrap;
-        justify-content: center;
-        align-items: center;
-        margin-bottom: 2rem;
-        width: 70%;
-    }
+	div {
+		display: flex;
+		gap: 1rem;
+		flex-wrap: wrap;
+		justify-content: center;
+		align-items: center;
+		margin-bottom: 2rem;
+		width: 70%;
+	}
 
- @media screen and (max-width: 600px) {
-			h3 {
-				font-size: 2rem;
-			}
+	@media screen and (max-width: 600px) {
+		h3 {
+			font-size: 2rem;
 		}
+	}
 </style>
-
