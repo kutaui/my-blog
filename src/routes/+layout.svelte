d3bd2704f148a4ac5213b213b2c949fc091f103c
kutaui <kutaybekleric@gmail.com>
2024-03-24 12:29:31 +0300

    posthog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import '../app.css';
+	import posthog from 'posthog-js';
 
 	import Header from '$lib/components/Header/Header.svelte';
 	import { theme } from '$lib/stores/theme';
 	import { browser } from '$app/environment';
 	import type { LayoutServerData } from '../../.svelte-kit/types/src/routes/$types';
 	import Footer from '$lib/components/Footer.svelte';
+
+	posthog.init('phc_r4q2OTqmSdtyl3zUGzLMrAc8KBW3lIkHVhdKMm60l9n', {
+		api_host: 'https://app.posthog.com'
+	});
 
 	export let data: LayoutServerData;
 
